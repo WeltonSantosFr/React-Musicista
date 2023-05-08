c384d6ee165c1eda6279ef418928d6344dcb5464
=Welton Santos <weltonsantosfr@gmail.com>
2023-05-08 14:04:06 -0300

    feat:footer-component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import { AiOutlineArrowUp } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark w-full h-[6.25rem] fixed bottom-0">
+      <div className="flex h-full w-4/5 justify-end items-center">
+        <button className="bg-[#000000] h-20 w-20 flex items-center justify-center">
+          <AiOutlineArrowUp className="text-text w-10 h-10" />
+        </button>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
