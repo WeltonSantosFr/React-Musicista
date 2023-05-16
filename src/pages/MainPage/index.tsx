d2db292d859a59cdc36e7166d9a381055d1a283b
=Welton Santos <weltonsantosfr@gmail.com>
2023-05-16 15:02:17 -0300

    wip:feat/main-page

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,116 +1,69 @@
-import { useState } from "react";
-import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { GoTriangleRight } from "react-icons/go";
 
 const MainPage = () => {
-  const [topic, setTopic] = useState<string | null>(null);
   return (
-    <div className="overflow-auto max-h-screen">
+    <div className="bg-gray3 h-screen flex flex-col items-center">
       <Header />
+      <div className="w-4/5 flex flex-col gap-4">
+        <h3 className="font-bold text-2xl text-text">
+          Bem vindo user, que tal explorar um pouco?
+        </h3>
 
-      <div className="bg-gray3 h-screen flex justify-center">
-        <div className="w-4/5 flex flex-col mt-12 gap-8">
-          <p className="font-bold text-2xl text-text">
-            Bem Vindo, que tal explorar um pouco?
-          </p>
-
-          <div className="text-text">
-            <p className="font-normal text-2xl">Conteúdos</p>
-            <div className="bg-gray1 h-[12.5rem] rounded-[0.5rem] flex justify-evenly items-center">
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("notes")}
-              >
+        <div className="flex flex-col gap-2 bg-gray2 rounded-lg h-80 justify-evenly">
+          <h2 className="flex font-semibold text-text">Guitarra e Violão</h2>
+          <details className="bg-text shadow rounded">
+            <summary className="list-none flex flex-wrap items-center cursor-pointer">
+              <h3 className="flex flex-1 p-4 font-semibold">Básico</h3>
+              <div className="flex w-10 items-center justify-center">
+                <GoTriangleRight />
+              </div>
+            </summary>
+            <div className="flex items-center justify-evenly">
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
                 Notas
               </button>
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("triads")}
-              >
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
                 Tríades
               </button>
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("tetrads")}
-              >
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
                 Tétrades
               </button>
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("major scale")}
-              >
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
                 Escala Maior
               </button>
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("minor scale")}
-              >
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
                 Escala Menor
               </button>
-              <button
-                className="bg-gray2 h-[11.25rem] w-[13.75rem] rounded-[0.5rem] hover:bg-gray3"
-                onClick={() => setTopic("harmonic field")}
-              >
-                Campo Harmonico
+              <button className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3">
+                Campo Harmônico
               </button>
             </div>
-          </div>
-          {topic === "notes" ? (
-            <div className="w-full flex flex-col justify-evenly self-center items-start overflow-scroll">
-              <h2 className="font-bold text-text text-5xl">
-                O que são as notas musicais afinal?
-              </h2>
-              <p className="text-text text-3xl">
-                As notas musicais são frequências específicas de vibração, que
-                geram um som quando são interpretadas pelo nosso cérebro, logo
-                após passar pelo sistema auditivo. Todo instrumento musical
-                (inclusive nossa voz, através das cordas vocais) vibra quando
-                tocamos. Quando essa vibração é regular, conseguimos ouvir uma
-                nota. Quando é irregular, costumamos chamar de barulho, porque
-                não é produzida uma nota definida.
-              </p>
-              <p className="text-text text-3xl">
-                Na música ocidental, nos baseamos em sete notas chamadas
-                naturais. Elas formam aquela velha sequência que aprendemos
-                desde a infância, o famoso Dó Ré Mi Fá Sol Lá Si (Dó). O
-                interessante é que essa sequência é cíclica e aparece em
-                quantidades determinadas em cada instrumento. Ou seja, quando
-                vemos aquele monte de teclas em um piano, ou as diversas cordas
-                e casas em um violão, temos a repetição dessas notas de diversas
-                formas. O que muda é que temos para cada nota a possibilidade de
-                tocá-la mais grave ou mais aguda. A frequência não é a mesma
-                entre um Dó grave e um Dó agudo, porém guardam uma proporção que
-                faz com que as identifiquemos ambas como Dó.
-              </p>
-              <p className="text-text text-3xl">
-                Se você estiver se perguntando como sabemos que uma nota está
-                afinada, usamos um padrão, que pode ser alterado dependendo da
-                situação. Nosso padrão de afinação de baseia na nota Lá, que
-                deve estar afinada em 440Hz. À partir desse padrão que as outras
-                notas são afinadas, conforme a distância específica entre as
-                notas.
-              </p>
+          </details>
+          <details className="bg-text shadow rounded">
+            <summary className="list-none flex flex-wrap items-center cursor-pointer">
+              <h3 className="flex flex-1 p-4 font-semibold">Intermediário</h3>
+              <div className="flex w-10 items-center justify-center">
+                <GoTriangleRight />
+              </div>
+            </summary>
+            <div className="flex items-center justify-evenly">
+              <p className="font-semibold">Em produção</p>
             </div>
-          ) : topic === "triads" ? (
-            <></>
-          ) : topic === "tetrads" ? (
-            <></>
-          ) : topic === "major scale" ? (
-            <></>
-          ) : topic === "minor scale" ? (
-            <></>
-          ) : topic === "harmonic field" ? (
-            <></>
-          ) : (
-            <div className="w-4/5 flex h-screen justify-center self-center">
-              <h2 className="font-bold text-text text-6xl w-[28.125rem]">
-                SELECIONE UM TÓPICO ACIMA
-              </h2>
+          </details>
+          <details className="bg-text shadow rounded">
+            <summary className="list-none flex flex-wrap items-center cursor-pointer">
+              <h3 className="flex flex-1 p-4 font-semibold">Avançado</h3>
+              <div className="flex w-10 items-center justify-center">
+                <GoTriangleRight />
+              </div>
+            </summary>
+            <div className="flex items-center justify-evenly">
+              <p className="font-semibold">Em produção</p>
             </div>
-          )}
+          </details>
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
+import MainPage from "../pages/MainPage";
 
 const RouteMain = () => {
   return (
@@ -10,6 +11,7 @@ const RouteMain = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
+        <Route path="home" element={<MainPage />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
