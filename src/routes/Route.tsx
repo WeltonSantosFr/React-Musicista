d423f182c6307278d4b202321fbaad5bcdfa5448
=Welton Santos <weltonsantosfr@gmail.com>
2023-05-25 14:36:04 -0300

    feat:lesson-notes-page

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import MainPage from "../pages/MainPage";
+import LessonNotesPage from "../pages/LessonNotesPage";
 
 const RouteMain = () => {
   return (
@@ -12,6 +13,7 @@ const RouteMain = () => {
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="home" element={<MainPage />} />
+        <Route path="lesson/notes" element={<LessonNotesPage />} />
         <Route path="*" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
