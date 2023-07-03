75a795fc1c092f0fc5534b556b6c02f96da99b60
=Welton Santos <weltonsantosfr@gmail.com>
2023-07-03 13:34:13 -0300

    fix landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <GoogleOAuthProvider clientId="1082995543427-e6gjpk1t8qhogibgsgk3ephegvii1nld.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </GoogleOAuthProvider>
+);
