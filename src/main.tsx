60ceddf9f8d3133438ac6b8d7dcc98bb63ad7ffe
Welton <weltonsantosfr@gmail.com>
2023-07-13 15:07:20 -0300

    remove google auth implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <GoogleOAuthProvider clientId="1082995543427-e6gjpk1t8qhogibgsgk3ephegvii1nld.apps.googleusercontent.com">
+  
     <React.StrictMode>
       <App />
     </React.StrictMode>
     ,
-  </GoogleOAuthProvider>
 );
