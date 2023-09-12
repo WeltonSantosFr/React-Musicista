43de55322b104cf7f35ae29c6efc716860c20328
Welton <weltonsantosfr@gmail.com>
2023-09-12 15:47:23 -0300

    chore: toast implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,29 @@
-import { FaGuitar } from "react-icons/fa";
+import { useState } from "react";
+import { FaGuitar, FaUserCircle } from "react-icons/fa";
 import { useNavigate } from "react-router";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [dropbar, setDropbar] = useState(false)
+
   return (
-    <header className="w-full bg-gray3 h-[6.25rem] flex items-center justify-center">
-      <div
-        className="flex h-full w-4/5 items-center justify-center lg:justify-start cursor-pointer"
-        onClick={() => navigate("/home")}
-      >
-        <FaGuitar className="text-text w-[3.125rem] h-[3.125rem]" />
-        <h1 className="text-text font-bold text-5xl">Musicista</h1>
+    <header className="w-4/5 h-full flex flex-col items-center">
+      <div className="flex items-center h-28 w-full justify-between">
+              <div className="flex items-center">
+              <FaGuitar className="text-text w-[1.563rem] h-[1.563rem]" />
+              <h1 className="text-text font-bold text-3xl">Musicista</h1>
+              </div>
+
+              {dropbar? <div className="w-24 h-20 rounded-md fixed top-[10%] right-[10%] bg-gray1 flex flex-col justify-evenly items-center">
+                <button className="w-full h-full text-text font-semibold hover:bg-dark text-center" onClick={() => navigate("")}>Meu Perfil</button>
+                <button className="w-full h-full text-text font-semibold hover:bg-dark text-center"  onClick={() => navigate("")}>Sair</button>
+              </div>: null}
+              <FaUserCircle onClick={() => setDropbar(!dropbar)} className="text-text w-[1.563rem] h-[1.563rem]"/>
       </div>
+
     </header>
   );
 };
 
 export default Header;
+
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { GoTriangleRight } from "react-icons/go";
@@ -5,192 +6,48 @@ import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
   const navigate = useNavigate();
+  const [lessons, setLessons] = useState(
+    [ {title:"O que são as notas musicais afinal?"}, 
+      {title:"O que são triades?"}, 
+      {title:"O que são tétrades?"}, 
+      {title:"O que são as escalas maiores?"}, 
+      {title:"O que são as menores?"}, 
+      {title:"O que é um campo harmonico?"}])
+
   return (
     <div className="bg-gray3 min-h-screen flex flex-col items-center">
       <Header />
       <div className="w-4/5 flex flex-col gap-4">
-        <h3 className="font-bold text-2xl text-text">
+
+        <div className="w-full flex flex-col sm:flex-row items-center md:justify-between gap-2.5 md:gap-0">
+        <h3 className="font-semibold text-base text-text self-start">
           Bem vindo user, que tal explorar um pouco?
         </h3>
 
-        <div className="flex flex-col gap-2 rounded-lg h-min justify-evenly">
-          <div className="h-1 bg-text"></div>
-          <h2 className="flex font-semibold text-text">Guitarra e Violão</h2>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Básico</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/notes")}
-              >
-                Notas
-              </button>
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/triads")}
-              >
-                Tríades
-              </button>
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/tetrads")}
-              >
-                Tétrades
-              </button>
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/major-scales")}
-              >
-                Escala Maior
-              </button>
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/minor-scales")}
-              >
-                Escala Menor
-              </button>
-              <button
-                className="w-44 h-10 mb-2 bg-gray2 rounded-md text-text font-semibold hover:bg-gray3"
-                onClick={() => navigate("/lesson/harmonic-field")}
-              >
-                Campo Harmônico
-              </button>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Intermediário</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Avançado</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <div className="h-1 bg-text"></div>
-          <h2 className="flex font-semibold text-text">Contrabaixo</h2>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Básico</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Intermediário</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Avançado</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <div className="h-1 bg-text"></div>
-          <h2 className="flex font-semibold text-text">Piano e Teclado</h2>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Básico</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Intermediário</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Avançado</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <div className="h-1 bg-text"></div>
-          <h2 className="flex font-semibold text-text">Bateria</h2>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Básico</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Intermediário</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
-          <details className="bg-text shadow rounded">
-            <summary className="list-none flex flex-wrap items-center cursor-pointer">
-              <h3 className="flex flex-1 p-4 font-semibold">Avançado</h3>
-              <div className="flex w-10 items-center justify-center">
-                <GoTriangleRight />
-              </div>
-            </summary>
-            <div className="flex items-center justify-evenly">
-              <p className="font-semibold">Em produção</p>
-            </div>
-          </details>
+        <div className="w-full flex justify-between md:justify-end gap-2 md:w-1/2">
+          <button disabled className="bg-text text-[#000] h-10 w-20 sm:w-24 rounded-xl font-medium disabled:opacity-50">Aulas</button>
+          <button disabled className="bg-text text-[#000] h-10 w-20 sm:w-24 rounded-xl font-medium disabled:opacity-50">Fórum</button>
+          <button disabled className="bg-text text-[#000] h-10 w-20 sm:w-24 rounded-xl font-medium disabled:opacity-50">Tabs</button>
         </div>
-      </div>
+        
+        </div>
+
+        <div className="w-full">
+          <div className="w-full h-16 flex flex-row justify-between items-center">
+            <p className="text-text text-lg font-bold">Aulas</p>
+            <button className="text-text text-lg font-bold">Acessar Página</button>
+          </div>
+
+          <div className="w-full flex overflow-x-auto justify-between gap-8 rounded-md">
+            {lessons.map((lesson) => 
+            <div className="border-2 border-text rounded-md flex flex-col justify-between min-w-[200px] max-w-[200px] min-h-[280px] max-h-[280px] items-center"> 
+              <img src="src\assets\Office Icon.jpg" alt="lessonImage" className="w-[200px]"/>
+              <p className="text-text font-semibold text-lg text-center">{lesson.title}</p>
+              <button className="w-3/4 h-8 bg-text rounded-md mb-1 text-[#000] font-semibold">Acessar Página</button>
+            </div>)}
+          </div>
+        </div>
+      </div> 
       <Footer />
     </div>
   );
